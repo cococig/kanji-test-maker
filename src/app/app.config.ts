44a3695b72aeb1c070350c10e18f564faaa3e8f7
cococig <84442548+cococig@users.noreply.github.com>
2024-01-01 08:22:09 +0000

    feat: PWA対応

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,10 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, isDevMode } from "@angular/core";
 import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideAnimations()],
+	providers: [provideAnimations(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })],
 };
